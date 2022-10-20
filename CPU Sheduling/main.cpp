96a335ee10a14f3b04d0930de99bc39f392828d7
Yogesh Mokal <mokalyogesh113@gmail.com>
2022-10-20 20:26:28 +0530

    Did changes in fcfs(), sjf() , priority() and defined round_robin();

--- CPU Sheduling/main.cpp
+++ CPU Sheduling/main.cpp
@@ -5,6 +5,7 @@ void fcfs();
 void sjf();
 void priority();
 void round_robin();
+int MAX = 30;
 
 int main()
 {
@@ -103,9 +104,6 @@ void fcfs()
     for (int i = 0; i <= n; i++)
         cout << gc[i] << "   ";
 
-    cout << "Process  BurstTime   WaitingTime     TurnAroundTime";
-    cout << " 3 p0       1             2                ";
-
     cout << "\n\nProcess  BurstTime   WaitingTime     TurnAroundTime";
     for (int i = 0; i < n; i++)
         cout << endl
@@ -116,12 +114,14 @@ void fcfs()
         sum += wt[i];
     float average_wt = float(sum) / n;
 
+    sum = 0;
     for (int i = 0; i < n; i++)
         sum += tat[i];
     float average_tat = float(sum) / n;
+    
 
     cout << "\nAverage Waiting Time = " << average_wt;
-    cout << "\nAverage TurnAround Time = " << average_wt;
+    cout << "\nAverage TurnAround Time = " << average_tat;
 }
 
 void sjf()
@@ -192,12 +192,13 @@ void sjf()
         sum += wt[i];
     float average_wt = float(sum) / n;
 
+    sum = 0;
     for (int i = 0; i < n; i++)
         sum += tat[i];
     float average_tat = float(sum) / n;
 
     cout << "\nAverage Waiting Time = " << average_wt;
-    cout << "\nAverage TurnAround Time = " << average_wt;
+    cout << "\nAverage TurnAround Time = " << average_tat;
 }
 
 void priority()
@@ -266,7 +267,6 @@ void priority()
     for (int i = 0; i <= n; i++)
         cout << gc[i] << "   ";
 
-
     cout << "\n\nProcess  BurstTime   WaitingTime     TurnAroundTime";
     for (int i = 0; i < n; i++)
         cout << endl
@@ -277,14 +277,107 @@ void priority()
         sum += wt[i];
     float average_wt = float(sum) / n;
 
+    sum = 0;
     for (int i = 0; i < n; i++)
         sum += tat[i];
     float average_tat = float(sum) / n;
 
     cout << "\nAverage Waiting Time = " << average_wt;
-    cout << "\nAverage TurnAround Time = " << average_wt;
+    cout << "\nAverage TurnAround Time = " << average_tat;
 }
 
 void round_robin()
 {
+    int n, qt;
+    cout << "Enter the Number of Processes ==> ";
+    cin >> n;
+
+    cout << "Enter the Quantum Time ==> ";
+    cin >> qt;
+
+    int bt[n], gc[MAX], gc_c = 0, ft[n], wt[n], tat[n], p[MAX], p_c = 0, count = 0, Max = 0;
+    gc[gc_c] = 0;
+    gc_c++;
+
+    cout << "\n*****Enter the Burst time of the processes*****\n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter the burst Time of the process p" << i << " ==> ";
+        cin >> bt[i];
+        Max = max(Max, bt[i]);
+        count += bt[i] == 0 ? 1 : 0;
+        p[i] = i;
+    }
+
+    // Finding the values of gc[]  required for gantt chart
+    int pass = Max / qt + (Max % qt != 0 ? 1 : 0);
+    int t_bt[n];
+    copy(bt, bt + n, t_bt); // This Function will copy the elements of bt[] into t_bt[]
+
+    for (int x = 0; x < pass; x++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (t_bt[i] != 0)
+            {
+                gc[gc_c] = gc[gc_c - 1] + min(t_bt[i], qt);
+                gc_c++;
+                t_bt[i] -= min(t_bt[i], qt);
+
+                p[p_c] = i;
+                p_c++;
+            }
+        }
+    }
+
+    // Finding the waiting time and turn around time of each process
+    for (int i = 0; i < n; i++)
+    {
+        int ft = 0;
+        for (int j = 1; j <= gc_c; j++)
+        {
+            if (p[j - 1] == i)
+            {
+                ft = j;
+            }
+        }
+
+        wt[i] = gc[ft] - bt[i];
+        tat[i] = wt[i] + bt[i];
+    }
+
+    cout << "\n\n  Gantt Chart";
+    cout << "\n+--";
+    for (int i = 1; i < p_c; i++)
+        cout << "-+--";
+    cout << "-+\n";
+
+    for (int i = 0; i < p_c; i++)
+        cout << "| p" << p[i];
+    cout << "|";
+
+    cout << "\n+--";
+    for (int i = 1; i < p_c; i++)
+        cout << "-+--";
+    cout << "-+\n";
+    for (int i = 0; i < gc_c; i++)
+        cout << gc[i] << "   ";
+
+    cout << "\n\nProcess  BurstTime   WaitingTime     TurnAroundTime";
+    for (int i = 0; i < n; i++)
+        cout << endl
+             << "   p" << p[i] << "       " << bt[i] << "             " << wt[i] << "                " << tat[i];
+
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+        sum += wt[i];
+    float average_wt = float(sum) / n;
+
+    sum = 0;
+    for (int i = 0; i < n; i++)
+        sum += tat[i];
+    float average_tat = float(sum) / n;
+
+    cout << "\nAverage Waiting Time = " << average_wt;
+    cout << "\nAverage TurnAround Time = " << average_tat;
 }
